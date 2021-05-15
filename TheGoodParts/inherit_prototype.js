0c11401ebdcace053e0c9b49e3b346a68cf703f2
iMac21.5-2012 <61crossroad@gmail.com>
2021-05-16 02:38:40 +0900

    inherit by prototype

---
+++ TheGoodParts/inherit_prototype.js
@@ -0,0 +1,38 @@
+var myMammal = {
+    name: 'Herb the Mammal',
+    get_name: function () {
+        return this.name;
+    },
+    says: function () {
+        return this.saying || '';
+    }
+};
+
+var myCat = Object.create(myMammal);
+myCat.name = 'Henrietta';
+myCat.saying = 'meow';
+myCat.purr = function (n) {
+    var i, s = '';
+    for (i = 0; i < n; i++) {
+        if (s) {
+            s += '-';
+        }
+        s += 'r';
+    }
+    return s;
+};
+myCat.get_name = function () {
+    return this.says() + ' ' + this.name + ' ' + this.says();
+}
+console.log(myCat.purr(4));
+console.log(myCat.get_name());
+
+var block = function () {
+    var oldScope = scope;
+    scope = Object.create(scope);
+
+    advance('{');
+    parse(scope);
+    advance('}');
+    scope = oldScope;
+}
