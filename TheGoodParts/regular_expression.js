f9d9503f62be0df0c57118d3e80d50d8e5b2117b
iMac21.5-2012 <61crossroad@gmail.com>
2021-05-18 00:58:51 +0900

    regular expression basic

---
+++ TheGoodParts/regular_expression.js
@@ -0,0 +1,41 @@
+var parse_url = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
+
+var url = "http://www.ora.com:80/goodparts?q#fragment";
+var result = parse_url.exec(url);
+
+var names = ['url', 'scheme', 'slash', 'host', 'port', 'path', 'query', 'hash'];
+var blanks = '      ';
+
+var i;
+for (i = 0; i < names.length; i++) {
+    console.log(names[i] + ':' + blanks.substring(names[i].length), result[i]);
+}
+
+
+var parse_number = /^-?\d+(?:\.\d*)?(?:e[+\-]?\d+)?$/i;
+
+var test = function (num) {
+    console.log(parse_number.test(num));
+};
+
+test('1');
+test('number');
+test('98.6');
+test('132.21.86.100');
+test('123.45E-67');
+test('123.45D-67');
+
+
+var my_regexp = /"(?:\\.|[^\\\"])*"/g;
+my_regexp = new RegExp("\"(?:\\.|[^\\\\\\\"])*\"", 'g');
+
+function make_a_matcher() {
+    return /a/gi;
+}
+
+var x = make_a_matcher();
+var y = make_a_matcher();
+x.lastIndex = 10;
+console.log(y.lastIndex); // 10? 0....??
+
+console.log("into".match(/in|int/));
