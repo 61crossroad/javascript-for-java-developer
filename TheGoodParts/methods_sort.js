cc24b949ec4d15af17853e0d1c9078f3167d54f1
iMac21.5-2012 <61crossroad@gmail.com>
2021-05-19 22:04:49 +0900

    sort, compare implemented

---
+++ TheGoodParts/methods_sort.js
@@ -0,0 +1,78 @@
+var n = [4, 8, 15, 16, 23, 42];
+n.sort();
+console.log(n);
+
+n.sort(function (a, b) {
+    return a - b;
+});
+console.log(n);
+
+var m = ['aa', 'bb', 'a', 4, 8, 15, 16, 23, 42];
+m.sort(function (a, b) {
+    if (a === b) {
+        return 0;
+    }
+    if (typeof a === typeof b) {
+        return a < b ? -1 : 1;
+    }
+    return typeof a < typeof b ? -1 : 1;
+});
+console.log(m);
+
+var by = function (name) {
+    return function (o, p) {
+        var a, b;
+        if (typeof o === 'object' && typeof p === 'object' && o && p) {
+            a = o[name];
+            b = p[name];
+            if (a === b) {
+                return 0;
+            }
+            if (typeof a === typeof b) {
+                return a < b ? -1 : 1
+            }
+            return typeof a < typeof b ? -1 : 1;
+        } else {
+            throw {
+                name: 'Error',
+                message: 'Expected an object when sorting by ' + name
+            };
+        }
+    };
+};
+
+var s = [
+    {first: 'Joe', last: 'Besser'},
+    {first: 'Moe', last: 'Howard'},
+    {first: 'Joe', last: 'DeRita'},
+    {first: 'Shemp', last: 'Howard'},
+    {first: 'Larry', last: 'Fine'},
+    {first: 'Curly', last: 'Howard'}
+]
+// s.sort(by('first'));
+// console.log(s);
+
+var byby = function (name, minor) {
+    return function (o, p) {
+        var a, b;
+        if (typeof o === 'object' && typeof p === 'object' && o && p) {
+            a = o[name];
+            b = p[name];
+            if (a === b) {
+                return typeof minor === 'function' ? minor(o, p) : 0;
+            }
+            if (typeof a === typeof b) {
+                return a < b ? -1 : 1
+            }
+            return typeof a < typeof b ? -1 : 1;
+        } else {
+            throw {
+                name: 'Error',
+                message: 'Expected an object when sorting by ' + name
+            };
+        }
+    };
+};
+
+s.sort(byby('last', byby('first')));
+console.log(s);
