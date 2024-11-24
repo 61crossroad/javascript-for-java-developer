22736f8b31d40d69c2067a56e2f28e812aee1db3
Kim Seunghwan <61crossroad@gmail.com>
2024-11-25 02:20:02 +0900

    array method for stack and queue

---
+++ modern-javascript-tutorial/stack_and_queue.js
@@ -0,0 +1,19 @@
+let f = ["apple", "orage", "pear"];
+console.log(f);
+console.log(f.pop());
+console.log(f);
+
+f.push("pear");
+console.log(f);
+
+console.log(f.shift());
+console.log(f);
+
+f.unshift('apple');
+console.log(f);
+
+f.push("cranberry", "raseberry");
+console.log(f);
+f.unshift("coffee", "coke");
+console.log(f);
+
