8b04393a5ab08be585a665b5dbe34e8830735649
Kim Seunghwan <61crossroad@gmail.com>
2024-12-02 07:48:57 +0900

    constructor function

---
+++ core-javascript/constructor_function.js
@@ -0,0 +1,9 @@
+var Cat = function(name, age) {
+    this.bark = 'meaa';
+    this.name = name;
+    this.age = age;
+};
+
+var choco = new Cat('Choco', 7);
+var nabi = new Cat('Nabi', 5);
+console.log(choco, nabi);
