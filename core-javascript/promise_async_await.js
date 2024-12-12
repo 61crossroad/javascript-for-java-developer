b1efea3220b085ae170309e7f4b36197a1e098a2
Kim Seunghwan <61crossroad@gmail.com>
2024-12-13 07:56:56 +0900

    promise async/await

---
+++ core-javascript/promise_async_await.js
@@ -0,0 +1,21 @@
+var addCoffee = function (name) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve(name);
+        }, 500);
+    });
+};
+var coffeeMaker = async function() {
+    var coffeeList = '';
+    var _addCoffee = async function(name) {
+        coffeeList += (coffeeList ? ', ' : '') + await addCoffee(name);
+    };
+
+    await _addCoffee('Espresso');
+    console.log(coffeeList);
+    await _addCoffee('Americano');
+    console.log(coffeeList);
+    await _addCoffee('Cafe Mocha');
+    console.log(coffeeList);
+};
+coffeeMaker();
